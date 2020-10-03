5ff14858bfc32be8f4678a08e9c01181697f3d8c
iam-akshay <jainakshay.aj98@gmail.com>
2020-10-03 12:29:30 +0530

    react element (single & block)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './App.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/*
+* Render with JSX
+*/
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Display single element
+const element_p = <p className="text_bold">Display single element using JSX and applied css</p>
+ReactDOM.render(element_p, document.getElementById("root"))
+
+// Work with multiple elements
+// JSX uses Babel compilation and it will take time for the compilation
+const blog_element = (
+  <div>
+    <h4>My Project</h4>
+    <p>Created a blog panel using multiple elements with the help of <b>JSX</b>.</p>
+  </div>
+)
+ReactDOM.render(blog_element, document.getElementById("root"))
+
+/*
+* Render without JSX (using React Element)
+*/
+
+// created element and used attribute (class)  and rendered the element 
+const single_element = React.createElement("p", {"className": "text_bold"}, "Single element without JSX (using react create element method)");
+ReactDOM.render(single_element, document.getElementById("root"))
+
+// block element creation and rendered
+const block_element = React.createElement("div", {"className": "text_bold"}, 
+  React.createElement("h4", null, "My Project"),
+  React.createElement("p", null, "Created a blog using block elements without using JSX (using react create element)")
+)
+ReactDOM.render(block_element, document.getElementById("root"))
